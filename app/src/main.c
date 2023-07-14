8e32836a81d4c3dca46dc242456294661c23cb3a
daniel grenehed <danielgrenehed@MBPsomtordaniel.lan>
2023-07-14 09:32:26 +0200

    compiles

--- app/src/main.c
+++ app/src/main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
 #include <zephyr/display/cfb.h>
-//#include <zephyr/logging/log.h>
+#include <zephyr/logging/log.h>
 
 /**
  *	nRF52840-Dongle:
@@ -38,16 +41,35 @@
  *		RX 		- GPIO 0.20
  *	
   * */
-//LOG_MODULE_REGISTER(display);
-//static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
+LOG_MODULE_REGISTER(display);
+static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
+
+
 
 void main(void) {
-	//if (display == NULL) {
-	//	LOG_ERR("device pointer is NULL");
-	//	return;
-	//}
-	//if (!device_is_ready(display)) {
-	//	LOG_ERR("display device is not ready");
-	//	return;
-	//}
+	if (display == NULL) {
+		LOG_ERR("device pointer is NULL");
+		return;
+	}
+	if (!device_is_ready(display)) {
+		LOG_ERR("display device is not ready");
+		return;
+	}
+
+	int ret;
+	ret = cfb_framebuffer_init(display);
+	if (ret != 0) {
+		LOG_ERR("could not initialize display");
+		return;
+	}
+	ret = cfb_print(display, "Hello, world!", 0, 0);
+	if (ret != 0) {
+		LOG_ERR("could not print to display!");
+		return;
+	}
+	ret = cfb_framebuffer_finalize(display);
+	if (ret != 0) {
+		LOG_ERR("could not finalize to display");
+		return;
+	}
 }
