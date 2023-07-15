99a6e29399e05992af0e4015da7289de2bb6c318
daniel grenehed <danielgrenehed@MBPsomtordaniel.lan>
2023-07-15 11:11:52 +0200

    gpio config

--- app/src/main.c
+++ app/src/main.c
@@ -12,23 +12,57 @@ static const struct gpio_dt_spec led2r = GPIO_DT_SPEC_GET(DT_NODELABEL(led2r), g
 static const struct gpio_dt_spec led2g = GPIO_DT_SPEC_GET(DT_NODELABEL(led2g), gpios);
 static const struct gpio_dt_spec led2b = GPIO_DT_SPEC_GET(DT_NODELABEL(led2b), gpios);
 
+static const struct gpio_dt_spec row1 = GPIO_DT_SPEC_GET(DT_NODELABEL(row1), gpios);
+static const struct gpio_dt_spec row2 = GPIO_DT_SPEC_GET(DT_NODELABEL(row2), gpios);
+static const struct gpio_dt_spec row3 = GPIO_DT_SPEC_GET(DT_NODELABEL(row3), gpios);
+static const struct gpio_dt_spec row4 = GPIO_DT_SPEC_GET(DT_NODELABEL(row4), gpios);
+
+static const struct gpio_dt_spec col1 = GPIO_DT_SPEC_GET(DT_NODELABEL(col1), gpios);
+static const struct gpio_dt_spec col2 = GPIO_DT_SPEC_GET(DT_NODELABEL(col2), gpios);
+static const struct gpio_dt_spec col3 = GPIO_DT_SPEC_GET(DT_NODELABEL(col3), gpios);
+static const struct gpio_dt_spec col4 = GPIO_DT_SPEC_GET(DT_NODELABEL(col4), gpios);
+
+static const struct gpio_dt_spec rota = GPIO_DT_SPEC_GET(DT_NODELABEL(rota), gpios);
+static const struct gpio_dt_spec rotb = GPIO_DT_SPEC_GET(DT_NODELABEL(rotb), gpios);
+
 LOG_MODULE_REGISTER(display);
 static const struct device *display = DEVICE_DT_GET(DT_NODELABEL(ssd1306));
 static const struct device *i2c = DEVICE_DT_GET(DT_NODELABEL(i2c0));
 
-
-void main(void) {
+void config_gpio() {
 	// configure leds
 	gpio_pin_configure_dt(&led1, GPIO_OUTPUT);
 	gpio_pin_configure_dt(&led2r, GPIO_OUTPUT);
 	gpio_pin_configure_dt(&led2g, GPIO_OUTPUT);
 	gpio_pin_configure_dt(&led2b, GPIO_OUTPUT);
 	
+	// config matrix col output
+	gpio_pin_configure_dt(&col1, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&col2, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&col3, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&col4, GPIO_OUTPUT);
+
+	//configure matrix row input
+	gpio_pin_configure_dt(&row1, GPIO_INPUT);
+	gpio_pin_configure_dt(&row2, GPIO_INPUT);
+	gpio_pin_configure_dt(&row3, GPIO_INPUT);
+	gpio_pin_configure_dt(&row4, GPIO_INPUT);
+
+	//configure rotary encoder
+	gpio_pin_configure_dt(&rota, GPIO_INPUT);
+	gpio_pin_configure_dt(&rotb, GPIO_INPUT);
+
 	// turn off all leds
 	gpio_pin_toggle_dt(&led1);
 	gpio_pin_toggle_dt(&led2r);
 	gpio_pin_toggle_dt(&led2g);
 	gpio_pin_toggle_dt(&led2b);
+	
+}
+
+void main(void) {
+
+	config_gpio();
 	
 	if (i2c == NULL || !device_is_ready(i2c)) {
 		LOG_ERR("i2c device failed");
